109a3b7eff08d306ad0c7c8f1510f5e054c0e71b
MuhammadUsamaAwan <underusama@gmail.com>
2024-02-13 17:06:51 +0500

    feat: signin with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,11 @@
+import { db } from '~/db';
+import { eq } from 'drizzle-orm';
 import NextAuth, { type User } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import Google from 'next-auth/providers/google';
+
+import { env } from '~/env';
+import { users } from '~/db/schema';
 
 export const {
   handlers: { GET, POST },
@@ -9,7 +14,10 @@ export const {
   signOut,
 } = NextAuth({
   providers: [
-    Google,
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       authorize: (credentials: User) => {
         return credentials;
@@ -18,5 +26,30 @@ export const {
   ],
   pages: {
     signIn: '/signin',
+  },
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === 'google') {
+        console.log('account', account);
+        console.log('profile', profile);
+        const email = profile?.email;
+        if (!email) {
+          throw new Error('No email found in profile');
+        }
+        const user = await db.query.users.findFirst({
+          where: eq(users.email, email),
+        });
+        if (user) {
+          return true;
+        }
+        await db.insert(users).values({
+          email,
+          status: 'active',
+          password: '',
+        });
+        return true;
+      }
+      return true;
+    },
   },
 });
